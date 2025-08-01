c513f3f6e902d2b2445f8d45e4d5041fb01397ca
biaiueoo <vebbyrizqy.11@gmail.com>
2025-08-01 16:33:24 +0700

    artikel entitiy

--- app/page.js
+++ app/page.js
@@ -19,7 +19,7 @@ export default function HomePage() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    fetch("https://backend.ptdahliaglobalindo.id/article")
+    fetch("https://backend.ptdahliaglobalindo.id/article?entity=RENTAL_MOTOR")
       .then((res) => res.json())
       .then((data) => {
         setArticles(data.slice(0, 3)); // Ambil 3 artikel pertama
