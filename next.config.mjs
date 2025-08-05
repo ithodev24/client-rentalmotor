b481bba774dafda513416d3176f7b862597b1333
dhianaomii <dhia.zahrah1511@gmail.com>
2025-08-05 09:22:58 +0700

    fix product

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['backend.ptdahliaglobalindo.id'],
+    domains: ["backend.ptdahliaglobalindo.id"],
   },
 };
 
